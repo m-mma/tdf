b53641176add5e2b3f8b2f6d63969a7c12b9da3f
m-mma <mark.muema@gmail.com>
2019-10-20 16:29:05 +0300

    added a robots file and updated readme and gulp files

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # TDF Staging Site
 Staging area for The Data Factory Website
+
 ## Set Up
 **You should have Git, Node, NPM and Gulp installed**
 1. Run `sudo npm init` and agree with all the configurations.
 2. Run `sudo npm install` to install the task dependencies.
 3. You can run `sudo npm-check-updates -u` or `sudo ncu -u` to update any old packages.
+
+## Gulp Tasks
+Running `gulp` watches for changes to the .html, .scss, .js and image files in the 'src' folder and compiles them into the root folder.
+
+*  `css-tasks` compiles the .scss into CSS and minifies it into the dist/css folder.
+*  `js-scripts` concatenates all the .js files in the js folder and minifies them into the dist/js folder.
+*  `html-minify` listens to .html changes in the src folder and minifies the file into the dist folder.
+*  `image-minify` watches for changes to PNG, JPEG, GIF and SVG images and compresses them into the dist/images folder.
+*  `generate-sw` generates a service worker and precaches static resources such as logos, images and embedded fonts.
---
+++ gulpfile.js
@@ -0,0 +1,127 @@
+'use strict';
+
+var gulp = require('gulp');
+var gulpwatch = require('gulp-watch');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var terser = require('gulp-terser');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+var swPrecache = require('sw-precache');
+var inlinesource = require('gulp-inline-source');
+
+
+//Watch for 'Ctrl + C' on Windows and end gulp process
+if (process.platform === "win32") {
+  require("readline")
+    .createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+    .on("SIGINT", function() {
+      process.emit("SIGINT");
+    });
+}
+process.on("SIGINT", function() {
+  process.exit();
+});
+
+// Start CSS tasks
+gulp.task('css-tasks', done => {
+  gulp.src('src/sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'],
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('css/'))
+  done();
+});
+// End CSS tasks
+
+// Start JS tasks
+gulp.task('js-scripts', done => {
+  gulp.src([
+      // 'src/js/jquery-3.3.1.min.js',
+      'src/js/test.js'
+    ])
+    .pipe(concat('all-js-min.js'))
+    .pipe(terser())
+    .pipe(gulp.dest('js/'))
+  done();
+});
+// End JS tasks
+
+
+// Start HTML minify task
+gulp.task('html-minify', done => {
+  return gulp.src(['src/**/*.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+      // removeEmptyElements: true,
+      minifyCSS: true,
+      minifyJS: true
+    }))
+    .pipe(gulp.dest('./'));
+  done();
+});
+// end HTML minify task
+
+
+// Start image minify task
+gulp.task('image-minify', () =>
+  gulp.src('src/images/**/*.*')
+  .pipe(imagemin([
+    imagemin.gifsicle({
+      interlaced: true
+    }),
+    imagemin.jpegtran({
+      progressive: true
+    }),
+    imagemin.optipng({
+      optimizationLevel: 5
+    }),
+  ]))
+  .pipe(gulp.dest('images/'))
+);
+// End  image minify task
+
+// Start service worker precache
+gulp.task('generate-sw', function(callback) {
+  var path = require('path');
+  // var swPrecache = require('sw-precache');
+  var rootDir = './';
+
+  swPrecache.write(`service-worker.js`, {
+    // staticFileGlobs: [rootDir + '/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    staticFileGlobs: [
+      // './**.html',
+      // 'css/main.css',
+      'css/fonts/**.*'
+      // 'images/site-icons/**.*',
+      // 'images/static_images/**.*'
+    ],
+    // stripPrefix: rootDir
+
+  }, callback);
+
+});
+
+// End service worker precache
+
+// Start watch task
+gulp.task('default', done => {
+  gulp.watch('src/js/*.js', gulp.series('js-scripts'));
+  gulp.watch('src/sass/**/*.scss', gulp.series('css-tasks'));
+  gulp.watch('src/*.html', gulp.series('html-minify'));
+  gulp.watch('src/images/**/*.*', gulp.series('image-minify'));
+  // gulp.watch( 'dist', gulp.series('generate-sw'));
+  done();
+});
