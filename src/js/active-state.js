9df421cd1f3abe6280d1e6b831969df30e0c61da
m-mma <mark.muema@gmail.com>
2019-11-02 16:35:14 +0300

    updates to the active state script

--- src/js/active-state.js
+++ src/js/active-state.js
@@ -4,13 +4,18 @@
 const links = document.querySelectorAll('.nav-links');
 const sections = document.querySelectorAll('.section-title');
 
+
 function changeLinkState() {
   let index = sections.length;
 
-  while (--index && window.scrollY + 150 < sections[index].offsetTop) {}
+  while (--index && window.scrollY + 135 < sections[index].offsetTop) {}
 
   links.forEach((link) => link.classList.remove('active'));
   links[index].classList.add('active');
+
+  if (window.scrollY==0) {
+    links[index].classList.remove('active');
+  }
 }
 
 changeLinkState();
