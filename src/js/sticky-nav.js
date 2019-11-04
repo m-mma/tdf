502873cc0eb77a52840c91c3d9123145657c46d4
m-mma <mark.muema@gmail.com>
2019-11-05 01:23:58 +0300

    added terms and privacy content

--- src/js/sticky-nav.js
+++ src/js/sticky-nav.js
@@ -4,13 +4,28 @@
 let headerNav = document.getElementById("header");
 let sticky = headerNav.offsetTop;
 let termsTOC = document.getElementById("toc-list");
+// let titles = document.querySelectorAll('.section-title');
 
-
+//Homepage Sticky Navigation
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
     headerNav.classList.add("sticky-header");
-    termsTOC.classList.add("sticky-toc");
     // console.log('added sticky');
+  } else {
+    headerNav.classList.remove("sticky-header");
+  }
+
+  if (window.pageYOffset <= sticky) {
+    headerNav.classList.remove("sticky-header");
+    // console.log('removed sticky');
+  }
+}
+//Terms & Privacy  - TOC Sticky Navigation
+function stickyNavToc() {
+  if (window.pageYOffset >= sticky) {
+    headerNav.classList.add("sticky-header");
+    termsTOC.classList.add("sticky-toc");
+    // console.log('added TOC sticky');
   } else {
     headerNav.classList.remove("sticky-header");
     termsTOC.classList.remove("sticky-toc");
@@ -19,6 +34,6 @@ function stickyNav() {
   if (window.pageYOffset <= sticky) {
     headerNav.classList.remove("sticky-header");
     termsTOC.classList.remove("sticky-toc");
-    // console.log('removed sticky');
+    // console.log('removed TOC sticky');
   }
 }
